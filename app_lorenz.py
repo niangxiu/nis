0f661d94048386ae9a202810710191800ddff184
niangxiu <niangxiu@gmail.com>
2019-06-19 18:08:30 -0700

    separate nilss main logic from particular applicaitions

---
+++ app_lorenz.py
@@ -0,0 +1,98 @@
+# use single direction and N tiem segments to solve Lorentz problem
+import numpy as np
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from nilss import *
+
+
+rho_lb = 20
+rho_ub = 26
+sigma = 10
+beta = 8. / 3.
+rho_arr = np.arange(rho_lb, rho_ub + 1)
+J_arr = np.zeros(rho_arr.shape)
+dJdrho_arr = np.zeros(rho_arr.shape)
+
+
+def ddt(uwvs, rho):
+    u = uwvs[0]
+    x, y, z = u
+    w = uwvs[1]
+    vstar = uwvs[2]
+    dudt = np.array([sigma * (y - x), x * (rho - z) - y, x * y - beta * z])
+    Df = np.array([[-sigma, sigma, 0],[rho - z,-1,-x],[y,x,-beta]])
+    dwdt = np.dot(Df, w.T)
+    dfdrho = np.array([0, x, 0])
+    dvstardt = np.dot(Df, vstar) + dfdrho
+    return np.array([dudt, dwdt.T, dvstardt])
+
+
+
+# parameter passing to nilss
+dt = 0.001
+nseg = 10 #number of segments on time interval
+T_seg = 2 # length of each segment
+T_ps = 10 # time of pre-smoothing
+nc = 3 # number of component in u
+nus = 1 # number of unstable direction
+
+
+# functions passing to nilss
+def dudt(u, rho):
+    x, y, z = u
+    return np.array([sigma * (y - x), x * (rho - z) - y, x * y - beta * z])
+
+
+def RK4(u, w, vstar, rho):
+    # integrate u, w, and vstar to the next time step
+    uwvs = np.array([u, w, vstar])
+    k0 = dt * ddt(uwvs, rho) 
+    k1 = dt * ddt(uwvs + 0.5 * k0, rho)
+    k2 = dt * ddt(uwvs + 0.5 * k1, rho)
+    k3 = dt * ddt(uwvs + k2, rho)
+    uwvs_new = uwvs + (k0 + 2*k1 + 2*k2 + k3) / 6.0
+    return uwvs_new
+
+
+def Euler(u, w, vstar, rho):
+    uwvs = np.array([u, w, vstar])
+    k0 = dt * ddt(uwvs, rho) 
+    uwvs_new = uwvs + k0 
+    return uwvs_new
+
+
+# main loop
+for i, rho in enumerate(rho_arr):
+    print(rho)
+    u0 =  (np.random.rand(nc)-0.5) * 100 + np.array([0,0,50]) #[-10, -10, 60]
+    J, dJdrho = nilss(dt, nseg, T_seg, T_ps, u0, nus, rho, Euler, dudt)
+    J_arr[i] = J
+    dJdrho_arr[i] = dJdrho
+
+
+# plot preparations
+plt.rc('axes', labelsize='xx-large',  labelpad=12)
+plt.rc('xtick', labelsize='xx-large')
+plt.rc('ytick', labelsize='xx-large')
+plt.rc('legend', fontsize='xx-large')
+plt.rc('font', family='sans-serif')
+
+
+# plot J vs r
+plt.figure(figsize=[12,12])
+plt.subplot(2,1,1)
+plt.plot(rho_arr, J_arr)
+plt.xlabel(r'$\rho$')
+plt.ylabel(r'$\langle J \rangle$')
+
+
+# plot dJdrho vs r
+plt.subplot(2,1,2)
+plt.plot(rho_arr, dJdrho_arr)
+plt.xlabel(r'$\rho$')
+plt.ylabel(r'$d \langle J \rangle / d \rho$')
+plt.ylim([0,2.0])
+plt.savefig('lorenz.png')
+plt.show()
--- nilss.py
+++ nilss.py
@@ -1,37 +1,12 @@
 # use single direction and N tiem segments to solve Lorentz problem
 import numpy as np
-import matplotlib as mpl
-from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-
-def ddt(uwvs):
-    u = uwvs[0]
-    [x, y, z] = u
-    w = uwvs[1]
-    vstar = uwvs[2]
-    dudt = np.array([sigma * (y - x), x * (rho - z) - y, x * y - beta * z])
-    Df = np.array([[-sigma, sigma, 0],[rho - z,-1,-x],[y,x,-beta]])
-    dwdt = np.dot(Df, w.T)
-    dfdrho = np.array([0, x, 0])
-    dvstardt = np.dot(Df, vstar) + dfdrho
-    return np.array([dudt, dwdt.T, dvstardt])
-
-def RK4(u, w, vstar):
-    # integrate u, w, and vstar to the next time step
-    #uwvs = np.array([u, w, vstar])
-    #k0 = dt * ddt(uwvs) 
-    #k1 = dt * ddt(uwvs + 0.5 * k0)
-    #k2 = dt * ddt(uwvs + 0.5 * k1)
-    #k3 = dt * ddt(uwvs + k2)
-    #uwvs_new = uwvs + (k0 + 2*k1 + 2*k2 + k3) / 6.0
-    uwvs = np.array([u, w, vstar])
-    k0 = dt * ddt(uwvs) 
-    uwvs_new = uwvs + k0 
-    return uwvs_new
     
 
-def pushSeg(nseg, nstep, nus, nc, dt, u0, vstar0, w0):
+def pushSeg(nseg, nstep, nus, nc, dt, u0, vstar0, w0, rho, integrator, dudt):
     # For lorentz problem
     # find u, w and vstar on each segment
 
@@ -51,10 +26,10 @@ def pushSeg(nseg, nstep, nus, nc, dt, u0, vstar0, w0):
     for iseg in range(0, nseg):
         for istep in range(0, nstep-1):
             u[iseg, istep+1], w[iseg, istep+1], vstar[iseg,istep+1]\
-                = RK4(u[iseg, istep], w[iseg, istep], vstar[iseg, istep])
+                = integrator(u[iseg, istep], w[iseg, istep], vstar[iseg, istep], rho)
         for istep in range(0, nstep):
-            [x, y, z] = u[iseg, istep]
-            f[iseg, istep] = [sigma * (y - x), x * (rho - z) - y, x * y - beta * z]
+            f[iseg, istep] = dudt(u[iseg, istep], rho)
+
 
         # get u, and renormalize v* and w for next segment
         if iseg < nseg - 1:
@@ -62,7 +37,6 @@ def pushSeg(nseg, nstep, nus, nc, dt, u0, vstar0, w0):
             
             [Qtemp, Rtemp] = (np.linalg.qr(w[iseg,-1].T, 'reduced'))
             w[iseg + 1, 0] = Qtemp.T
-            #w[iseg + 1, 0,0] = w[iseg, -1,0] / np.linalg.norm(w[iseg,-1,0])
 
             vstar[iseg + 1,0] = vstar[iseg,-1] 
             for ius in range(0, nus):
@@ -71,43 +45,28 @@ def pushSeg(nseg, nstep, nus, nc, dt, u0, vstar0, w0):
                     / np.dot(w[iseg + 1,0,ius], w[iseg + 1,0,ius]) \
                     * w[iseg + 1,0,ius]
 
+
     # calculate vstar_perp and w_perp
     for iseg in range(0, nseg):
         for i in range(0, nstep):
             vstar_perp[iseg, i] = vstar[iseg, i] - np.dot(vstar[iseg, i], f[iseg, i]) / np.dot(f[iseg, i], f[iseg, i]) * f[iseg, i]
             for ius in range(0, nus):
-                w_perp[iseg, i, ius] = w[iseg, i,ius] - np.dot(w[iseg, i, ius],     f[iseg, i]) / np.dot(f[iseg, i], f[iseg, i]) * f[iseg, i]
+                w_perp[iseg, i, ius] = w[iseg, i,ius] - np.dot(w[iseg, i, ius], f[iseg, i]) / np.dot(f[iseg, i], f[iseg, i]) * f[iseg, i]
 
     return [u, w, vstar, w_perp, vstar_perp, f]
 
-rho_lb = 61
-rho_ub = 81
-nseg = 50 #number of segments on time interval
-T_seg = 2 # length of each segment
-rho = 0 # make a global variable
-sigma = 10
-beta = 8. / 3.
-Nrho = rho_ub - rho_lb + 1 # number of rho to be calculated
-J_arr = np.zeros(Nrho)
-dJdrho_arr = np.zeros(Nrho)
-rho_arr = np.zeros(Nrho)
-grow_rate_arr = np.zeros(Nrho) # the grow rate (10^*) in a time unit for w
 
-T_ps = 50 # time of pre-smoothing
-dt = 0.001
-nc = 3 # number of component in u
-nus = 2 # number of unstable direction
+def nilss(dt, nseg, T_seg, T_ps, u0, nus, rho, integrator, dudt):
 
-for rho in range(rho_lb, rho_ub + 1):
 
-    print(rho)
+    nc = len(u0)
     nstep = int(T_seg / dt) # number of step in each time segment
     v = np.zeros([nseg, nstep, nc])
     v_perp = np.zeros_like(v)
     ksi = np.zeros([nseg, nstep])
     
+
     #  get u0, vstar0, w0 for pre-smoothing
-    u0 =  (np.random.rand(nc)-0.5) * 100 + np.array([0,0,50]) #[-10, -10, 60]
     vstar0 = [0.0, 0.0, 0.0]
     w0 = np.zeros([nus,nc])
     for ius in range(0,nus):
@@ -115,7 +74,7 @@ for rho in range(rho_lb, rho_ub + 1):
         w0[ius] = w0[ius] / np.linalg.norm(w0[ius])
     # push forward u to a stable attractor
     nseg_ps = int(T_ps / dt / nstep)
-    [u_ps, w_ps, vstar_ps, temp1, temp2, temp3] = pushSeg(nseg_ps, nstep, nus, nc, dt,u0, vstar0, w0)
+    [u_ps, w_ps, vstar_ps, temp1, temp2, temp3] = pushSeg(nseg_ps, nstep, nus, nc, dt, u0, vstar0, w0, rho, integrator, dudt)
     # get initial value for later integration
     u0 = u_ps[-1,0]
     vstar0 = vstar_ps[-1,0]
@@ -123,9 +82,7 @@ for rho in range(rho_lb, rho_ub + 1):
 
 
     # find u, w, vstar on all segments
-    [u, w, vstar, w_perp, vstar_perp, f] = pushSeg(nseg, nstep, nus, nc, dt,u0, vstar0, w0)
-    # calculate grow rate of w
-    grow_rate_arr[rho - rho_lb] = (np.log(np.linalg.norm(w[-1,-1]) / np.linalg.norm(w[-1,0]))) / T_seg
+    [u, w, vstar, w_perp, vstar_perp, f] = pushSeg(nseg, nstep, nus, nc, dt,u0, vstar0, w0, rho, integrator, dudt)
     # construct M
     M = np.zeros([(2 * nseg - 1) * nus, (2 * nseg - 1) * nus])
     rhs = np.zeros((2 * nseg - 1) * nus)
@@ -142,6 +99,7 @@ for rho in range(rho_lb, rho_ub + 1):
             # rhs 1
             rhs[ii] = np.dot(vstar[iseg + 1,0] - vstar[iseg,-1], w[iseg + 1,0,ius])
     
+
     # dL/dlbd = 0: next nus*N equations
     for iseg in range(0, nseg):
         for ius in range(0, nus):
@@ -167,10 +125,12 @@ for rho in range(rho_lb, rho_ub + 1):
     #plt.spy(M)
     #plt.show()
 
+
     #lbd = np.linalg.solve(M, rhs)
     lbd = np.linalg.lstsq(M, rhs)
     lbd = lbd[0][:nseg * nus]
     lbd = lbd.reshape([nseg, nus])
+
 
     # calculate v
     for iseg in range(0, nseg):
@@ -180,20 +140,12 @@ for rho in range(rho_lb, rho_ub + 1):
             v[iseg] += lbd[iseg, ius] * w[iseg,:, ius,:]
             v_perp[iseg] += lbd[iseg, ius] * w_perp[iseg,:, ius,:]
     
+
     # calculate ksi
     for iseg in range(0, nseg):
         for i in range(0, nstep):
             ksi[iseg,i] = np.dot(v[iseg, i], f[iseg, i]) / np.dot(f[iseg, i], f[iseg, i])
-       
 
-    # window function
-    def window(eta):
-        w = 2 * (1 - np.cos(2 * np.pi * eta) ** 2)
-        return w
-    # calculate rho and dJ/drho
-    rho_arr[rho - rho_lb] = rho
-    J_arr[rho - rho_lb] = np.einsum(u[:,:,2],[0,1],[]) / (nstep * nseg)
-    t = np.zeros([nseg * (nstep - 1) + 1])
 
     # reshape v, v_perp, f, ksi to [nseg*(nstep-1), nc] vector: delete duplicate
     v_resu = np.zeros([nseg * (nstep - 1) + 1,nc])
@@ -209,38 +161,29 @@ for rho in range(rho_lb, rho_ub + 1):
             v_resu_perp[ii] = v_perp[iseg, istep]
             f_resu[ii] = f[iseg, istep]
             ksi_resu[ii] = ksi[iseg, istep]
-            t[ii] = ii * dt
     u_resu[-1] = u[-1,-1]
     v_resu[-1] = v[-1,-1]
     v_resu_perp[-1] = v_perp[-1,-1]
     f_resu[-1] = f[-1,-1]
     ksi_resu[-1] = ksi[-1,-1]
 
-    # apply windowing
-    T_total = nseg * (nstep - 1) * dt
-    t[-1] = T_total
-    
-    ## with window
-    #wdw = window(t / T_total)    
-    #dJdrho_arr[rho - rho_lb] = np.einsum(v_resu[:,2],[0],wdw,[0],[]) / (nseg * (nstep - 1) + 1) \
-    #                        - (ksi_resu[-1]*u[-1,-1,2] -  ksi_resu[0]*u[0,0,2]) / T_total \
-    #                        + (ksi_resu[-1] - ksi_resu[0]) * np.sum(u[:,:,2]) / (nseg * (nstep - 1) + 1) / T_total
 
-    ## with dilation
-    #dJdrho_arr[rho - rho_lb]= np.sum(v_resu_perp[:,2]) / (nseg * (nstep - 1) + 1) \
-    #                        - (ksi_resu[-1] * u_resu[-1,2] -  ksi_resu[0] * u_resu[0,2]) / T_total \
-    #                        + (ksi_resu[-1] - ksi_resu[0]) * np.sum(u_resu[:,2]) / (nseg * (nstep - 1) + 1) / T_total \
-    #                        + np.sum(f_resu[:,2] * ksi_resu) / (nseg * (nstep - 1) + 1)  
-    
-    # with dilation, with special care on fixed point # no done
+    # compute sensitivity, with special care on fixed point # no done
+    N_step = nseg * (nstep - 1) + 1
+    T_total = N_step * dt
     if np.sum((u_resu[-1000:,2] -np.average(u_resu[-1000:,2]))**2)  < 1e-6 * np.sum(u_resu[-1000:,2] **2):
-        dJdrho_arr[rho - rho_lb] = v_resu[-1,2]
+        dJdrho = v_resu[-1,2]
     else:
-        dJdrho_arr[rho - rho_lb]= np.sum(v_resu_perp[:,2]) / (nseg * (nstep - 1) + 1) \
-                            - (ksi_resu[-1] * u_resu[-1,2] -  ksi_resu[0] * u_resu[0,2]) / T_total \
-                            + (ksi_resu[-1] - ksi_resu[0]) * np.sum(u_resu[:,2]) / (nseg * (nstep - 1) + 1) / T_total \
-                            + np.sum(f_resu[:,2] * ksi_resu) / (nseg * (nstep - 1) + 1)   
-    pass                       
+        dJdrho = np.sum(v_resu_perp[:,2]) / N_step \
+                - (ksi_resu[-1] * u_resu[-1,2] -  ksi_resu[0] * u_resu[0,2]) / T_total \
+                + (ksi_resu[-1] - ksi_resu[0]) * np.sum(u_resu[:,2]) / N_step / T_total \
+                + np.sum(f_resu[:,2] * ksi_resu) / N_step   
+
+
+    # calculate Javg
+    Javg = np.einsum(u[:,:,2],[0,1],[]) / (nstep * nseg)
+
+    return Javg, dJdrho
 
     
     ## plot u
@@ -261,32 +204,3 @@ for rho in range(rho_lb, rho_ub + 1):
     #    othor[i] = np.dot(v_resu_perp[i,:], f_resu[i,:])
     #plt.plot(np.abs(othor))
     #plt.show()
-
-plt.rc('text', usetex=True)
-font = {'family' : 'normal',
-        'weight' : 'bold',
-        'size'   : 32}
-plt.rc('font', **font)
-
-# plot J vs r
-plt.subplot(2,1,1)
-plt.plot(rho_arr, J_arr)
-plt.xlabel(r'$\rho$')
-plt.ylabel(r'$\langle J \rangle$')
-
-# plot dJdrho vs r
-plt.subplot(2,1,2)
-plt.plot(rho_arr, dJdrho_arr)
-plt.xlabel(r'$\rho$')
-plt.ylabel(r'$d \langle J \rangle / d \rho$')
-plt.ylim([0,2.0])
-plt.savefig('withDilation_T2500.png')
-plt.show()
-
-# plot growrate vs r
-#plt.subplot(2,1,1)
-#plt.plot(rho_arr,grow_rate_arr)
-#plt.xlabel(r'\rho')
-#plt.ylabel('growRate')
-#plt.show()
-print('end')
